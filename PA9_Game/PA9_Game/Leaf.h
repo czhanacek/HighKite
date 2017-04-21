49716d2641c290f7ff4b1a77f542fc742e0130cc
Charlie Hanacek <charlie@czhanacek.com>
2017-04-20 19:50:50 -0700

    Fixed some virtuals that weren't supposed to be there

--- PA9_Game/PA9_Game/Leaf.h
+++ PA9_Game/PA9_Game/Leaf.h
@@ -3,11 +3,11 @@
 class Leaf : public DrawableWithPriority {
     public:
         Leaf(std::string newContext);
-        virtual void update(sf::Time totalElapsed, sf::Time sinceLastUpdate);
-        virtual Message react(sf::Event e);
-        virtual void receiveMessage(Message msg);
-        virtual Message click();
-        virtual Message unclick();
+        void update(sf::Time totalElapsed, sf::Time sinceLastUpdate);
+        Message react(sf::Event e);
+        void receiveMessage(Message msg);
+        Message click();
+        Message unclick();
 
 
 };

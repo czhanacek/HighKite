8ef5ab5ffb561dbae6ae2a85f7c991a851f029e8
Peter Brennan <peter.brennan@wsu.edu>
2017-04-22 13:08:07 -0700

    got the KiteObj class to compile

--- PA9_Game/PA9_Game/KiteObj.cpp
+++ PA9_Game/PA9_Game/KiteObj.cpp
@@ -3,10 +3,37 @@
 #include "KiteObj.h"
 
 
-KiteObj::KiteObj()
+KiteObj::KiteObj() : DrawableWithPriority("Kite", "Game", 50)
+{
+
+}
+
+KiteObj::~KiteObj()
+{
+
+}
+
+void KiteObj::update(sf::Time t)
 {
 
 
+}
+Message KiteObj::react(sf::Event e)
+{
 
+}
+
+void KiteObj::receiveMessage(Message msg)
+{
+
+}
+
+Message KiteObj::click()
+{
+
+}
+
+Message KiteObj::unclick()
+{
 
 }
--- PA9_Game/PA9_Game/KiteObj.h
+++ PA9_Game/PA9_Game/KiteObj.h
@@ -2,12 +2,17 @@
 #include "DrawableWithPriority.h"
 
 
-class KiteObj
+class KiteObj : public DrawableWithPriority
 {
 
 public:
     KiteObj();
     ~KiteObj();
+    virtual void update(sf::Time t);
+    virtual Message react(sf::Event e);
+    virtual void receiveMessage(Message msg);
+    virtual Message click();
+    virtual Message unclick();
 
 private:
 
